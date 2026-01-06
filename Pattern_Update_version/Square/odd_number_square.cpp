91c934763031d82a45326dbab2d03bb264a25eac
Anas <gazianas2020@gmail.com>
2026-01-06 19:38:39 +0600

    Refactor odd number square pattern logic
    Updated the logic to print odd numbers in a square pattern.

--- Pattern_Update_version/Square/odd_number_square.cpp
+++ Pattern_Update_version/Square/odd_number_square.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 /*
 4
-
-1
-1 3
-1 3 5
+1 3 5 7 
+1 3 5 7
+1 3 5 7
 1 3 5 7
 */
 
@@ -13,9 +12,11 @@ int main()
 {
     int n;
     cin>>n;
-    for(int i=0; i<=n; i++){
-      for(int j=1; j<=n; j++){
-        cout<<j++<<" ";
+    for(int i=1; i<=n; i++){ //rows
+      for(int j=1; j<=n*2-1; j= j+2){ //number of odd numbers in each row
+        // j=1,3,5,7...
+        //j= n*2-1 because nth odd number is 2n-1
+        cout<<j<<" ";
       }
       cout<<endl;
     }
